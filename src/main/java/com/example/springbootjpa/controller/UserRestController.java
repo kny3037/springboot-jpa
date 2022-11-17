818314da601fed0f8ef59b0d8d69bd5159f97cf3
psi5059 <psi5059@naver.com>
2022-11-18 00:43:43 +0900

    ResponseEntity

--- src/main/java/com/example/springbootjpa/controller/UserRestController.java
+++ src/main/java/com/example/springbootjpa/controller/UserRestController.java
@@ -20,6 +20,11 @@ public class UserRestController {
     public ResponseEntity<UserResponse> getUser(@PathVariable Long id) {
         return ResponseEntity.ok().body(userService.getUser(id));
     }
+    // ResponseEntity는 httpEntity를 상속받는, 결과 데이터와 HTTP 상태 코드를 직접 제어할 수 있는 클래스이다.
+    //HttpEntity를 상속 받고 사용자의 응답 데이터가 포함된 클래스이기 때문에
+    //HttpStatus, HttpHeaders, HttpBody를 포함.
+    //HttpHeaders : 요청/응답에 대한 요구사항이 적혀있다.
+    //HttpBody : 요구사항에 대한 내용이 적혀있다..?
 
     @PostMapping
     public ResponseEntity<UserResponse> addUser(@RequestBody UserRequest dto) {
